a4e5d54e2a9b58ea0e81aa975f8211486ccd4b42
pkdism <pawan.dwivedi94@gmail.com>
2019-09-15 00:12:21 +0530

    2019-02-11 challenge 375 easy main string more simplified

--- 375-easy-add-one-to-digit.py
+++ 375-easy-add-one-to-digit.py
@@ -1,6 +1,5 @@
 def add1_string(num):
-    res = ""
-    return res.join([res + (str(int(i) + 1)) for i in str(num)])
+    return ''.join(['' + (str(int(i) + 1)) for i in str(num)])
 
 print(add1_string(9))
 print(add1_string(998))
