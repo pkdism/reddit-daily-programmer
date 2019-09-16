aff7b9cc934e3b77a83a7105522d201dbc3ca44f
pkdism <pawan.dwivedi94@gmail.com>
2019-09-16 18:21:18 +0530

    2019-02-11 challenge 375 easy main numeric

--- 375-easy-add-one-to-digit.py
+++ 375-easy-add-one-to-digit.py
@@ -1,5 +1,19 @@
 def add1_string(num):
     return ''.join(['' + (str(int(i) + 1)) for i in str(num)])
 
+def add1_num(num):
+    res = 0
+    i = 0
+    while num > 0:
+        if num % 10 == 9:
+            res = (100 ** i) + res
+        else:
+            res = (10 ** i) * (num % 10 + 1) + res
+        i += 1
+        num = num // 10
+    return res
+
 print(add1_string(9))
 print(add1_string(998))
+print(add1_num(9))
+print(add1_num(998))
