5f541c19276f86610d9ad097cee32bf9368fbfbc
pkdism <pawan.dwivedi94@gmail.com>
2019-10-20 12:29:25 +0530

    2018-02-13 challenge 351 easy main

--- 351-easy-cricket-scoring.py
+++ 351-easy-cricket-scoring.py
@@ -12,7 +12,6 @@ for i in s:
         balls = 0
     if i >= '1' and i <= '9':
         scores[striker] += int(i)
-        # print('run ', int(i))
         if int(i)%2 == 1:
             striker, non_striker = non_striker, striker
         balls += 1
@@ -28,11 +27,6 @@ for i in s:
         striker += max(striker, non_striker) + 1
         balls += 1
         n_players += 1
-    # print(balls)
-    # print(striker)
-    # print(non_striker)
-    # print(scores)
-    # print("")
 
 for i in range(n_players+1):
     print('P' + str(i+1) + ": " + str(scores[i]))
